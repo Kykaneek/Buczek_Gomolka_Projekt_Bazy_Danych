ab9afc79a452ae74667752f9197b611e2215ba55
BuczuJBU <114801965+BuczuJBU@users.noreply.github.com>
2023-04-03 13:11:08 +0200

    API

--- Bazydanych/ClientApp/src/app/app.component.ts
+++ Bazydanych/ClientApp/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component,NgModule } from '@angular/core';
+import { LoginService } from './services/login.service'
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
+
+
 export class AppComponent {
   title = 'app';
 }
--- Bazydanych/ClientApp/src/app/login/login.component.ts
+++ Bazydanych/ClientApp/src/app/login/login.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginService } from '../services/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  auth: any;
+
+  constructor(private LoginService: LoginService) { }
   isExpanded = false;
-
-  collapse() {
-    this.isExpanded = false;
+  ngOnInit() {
+    this.auth = this.LoginService.getAllUser();
   }
-
-  toggle() {
-    this.isExpanded = !this.isExpanded;
-  }
+  
 }
